1e67feaa0e628d20cc383e4423ced6dc6d341cf4
Yana Tergueva <vivaldi_98@mail.ru>
2018-12-14 17:22:48 +0300

    opisanie

--- Prog.cpp
+++ Prog.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 int main()
 {
+	double a, b;
+    char c;
+    cout << "Input expression like: a*b, * as C; C can be '*','/','+','-'" << endl;
+    cin >> a;
+    cin >> c;
+    cin >> b;
 
     }
 system("pause");
