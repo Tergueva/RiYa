4ed16d43f7a9402641a0f76ccdbe74bb0ef66021
Yana Tergueva <vivaldi_98@mail.ru>
2018-12-14 16:46:28 +0300

    first

---
+++ funcs.cpp
@@ -0,0 +1,23 @@
+//funcs.cpp
+#include "funcs.h"
+double UMN(double a, double b)
+{
+	return a*b;
+}
+double DEL(double a, double b)
+{
+	if (b==0){return 0;}
+		else{return a/b;}
+}
+
+double SLOZH(double a, double b)
+{
+return a + b;
+}
+
+double VICH(double a, double b)
+{
+return a - b;
+}
+
+
